ecf6947df04724a96cd90fbbd436f0fb00ed2902
gilaco <gilad.co@gmail.com>
2023-05-25 00:23:10 +0300

    initial release

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS OpenAI Starter</title>
+      </Head>
+      <h1>Welcome to the Next JS &amp; OpenAI Starter</h1>
+    </div>
+  );
+}
